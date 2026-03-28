ba1aca43355f2b178c922c73b87ce7f5bf814e10
Elijah Rogers <eroger25@kent.edu>
2026-03-28 16:28:40 -0400

    Working on authentication

--- API Setup/server.js
+++ API Setup/server.js
@@ -1,3 +1,24 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+//Connection pool
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // required for Azure PostgreSQL
+  },
+});
+
+pool.connect()
+  .then(() => console.log("Connected to PostgreSQL!"))
+  .catch(err => {
+  console.error("Connection error:", err.message);
+});
+
+
 const express = require("express");
 const cors = require("cors");
 
@@ -5,20 +26,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let contracts = [
-  { id: 1, name: "Team Creation Portal" },
-  { id: 2, name: "Upload Refactor" }
-];
+app.get("/contracts", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM contracts");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
 
 // Test route
 app.get("/", (req, res) => {
   res.send("Project Tracker API Running");
 });
 
-// Get contracts
-app.get("/contracts", (req, res) => {
-  res.json(contracts);
-});
+
 
 // Add contract
 app.post("/contracts", (req, res) => {
